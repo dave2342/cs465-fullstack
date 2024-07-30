015dafcb199199ddf8eedc830fbd1f497af5feeb
David Varella <david.varella@snhu.edu>
2024-07-29 20:40:50 -0400

    Module 5 completd baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,13 +1,34 @@
-var fs = require('fs');
-//Read trips data from JSON file
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+}
+//GET travel view
+const travel = async function(req, res, next) {
+    //console.log('TRAVEL CONTROLLER BEGIN');
+    await fetch(tripsEndpoint, options)
+        .then(res => res.json())
+        .then(json => {
+            //console.log(json);
+            let message = null;
+            if(!(json instanceof Array)) {
+                message = 'API lookup error';
+                json = [];
+            } else {
+                if(!json.length){
+                    message = 'No trips exist in our database!';
 
-/* GET travel view */
-const travel = (req, res) => {
-    //pass trips data to handlebars
-    res.render('travel', {title: "Travlr Getaways", trips});
+                }
+            }
+            res.render('travel', {title: 'Travlr Getaways', trips: json, message});
+        })
+        .catch(err => res.status(500).send(e.message));
+        //console.log('TRAVEL CONTROLLER AFTER RENDER');
 };
 
 module.exports = {
     travel
 };
+
